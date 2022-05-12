8063757f712abcccb9e2a0a255e0b53b01c67fb8
Charlotte Marion <marion.charlotte@orange.fr>
2022-05-12 10:30:39 +0200

    mise à jour validation user et tournoi

--- app/models/tournoi.rb
+++ app/models/tournoi.rb
@@ -1,2 +1,3 @@
 class Tournoi < ApplicationRecord
+  validates :nom, :ville, :date_tournoi, :inscription_max, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  enum role: %i[admin dirigeant joueur]
+  validates_presence_of :nom, :prenom
+  validates_uniqueness_of :num_licence, allow_blank: true
 end
