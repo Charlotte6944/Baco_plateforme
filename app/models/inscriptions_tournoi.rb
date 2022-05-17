ca17ad41497263692c323efa86b616920d7cf8c7
Charlotte Marion <marion.charlotte@orange.fr>
2022-05-17 17:01:22 +0200

    modification model inscription_tournoi pour une seule combinaison user_id et tournoi_id

--- app/models/inscriptions_tournoi.rb
+++ app/models/inscriptions_tournoi.rb
@@ -2,4 +2,5 @@ class InscriptionsTournoi < ApplicationRecord
   belongs_to :user
   belongs_to :tournoi
   validates :date_inscription, :user_id, :tournoi_id, presence: true
+  validates :user_id, uniqueness: { scope: :tournoi_id }
 end
